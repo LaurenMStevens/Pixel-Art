2898cce6ebd9dcd868ab5eb0f3091857680d8378
LaurenMStevens <83794300+LaurenMStevens@users.noreply.github.com>
2021-06-18 21:35:41 -0400

    Update designs.js

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -1,11 +1,17 @@
 // Select color input
+
 var colorPicker = document.querySelector('#colorPicker');
 
 // Select size input
+
 var table = document.getElementById('pixelCanvas');
 
-
 var sizePicker = document.getElementById('sizePicker');
+
+//Add event listener to Size Picker to allow the user to
+//interact with the table specifications.
+// When size is submitted by the user, call makeGrid().
+
 
 sizePicker.addEventListener('submit', function(event){
     event.preventDefault();
@@ -14,10 +20,16 @@ sizePicker.addEventListener('submit', function(event){
     makeGrid(height,width);
 });
 
-// When size is submitted by the user, call makeGrid()
+//Clear any previous table data on 'submit'
 
+//Nested loop will loop through rows and columns to
+//construct table based on height/width input from user.
+
+//Adding event listener to table to allow user to
+//modify cell color based on input
 
 function makeGrid(height,width) {
+    table.innerHTML = null;
     for (var i = 0; i < height; i++){
         var row = table.insertRow(i);
         for (var j = 0; j < width; j++){
@@ -29,6 +41,5 @@ function makeGrid(height,width) {
     })
 
 };
-
 
 
